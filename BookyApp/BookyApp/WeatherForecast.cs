d3e1a940ba45662f2425156c0ac71a2b5594e972
m7md66 <mohamedal3alme6@gmail.com>
2024-06-26 22:23:03 +0300

    ffffffff

--- BookyApp/BookyApp/WeatherForecast.cs
+++ BookyApp/BookyApp/WeatherForecast.cs
@@ -5,6 +5,7 @@ namespace BookyApp
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
+        public int TemperaturC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
