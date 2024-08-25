e83809b84ab6a1284cc77d4adee54c2a954e12dc
mohamed essam <moessamn@gmail.com>
2024-08-25 17:26:51 +0300

    mooooooooo

--- BookyApp/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ BookyApp/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821606044c5ebd3ad00546e4c9c99617f622a279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0534b76d85bc9ca3d383b5293e68ba69c6942c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BookyApp/BookyApp/obj/Debug/net8.0/BookyApp.AssemblyInfo.cs
+++ BookyApp/BookyApp/obj/Debug/net8.0/BookyApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821606044c5ebd3ad00546e4c9c99617f622a279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0534b76d85bc9ca3d383b5293e68ba69c6942c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
